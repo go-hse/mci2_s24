5d5cc19cfbc404dd9018e86e0aa4bc07958dc8d4
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2024-03-14 16:36:12 +0100

    mini pong

--- 02_Canvas_Basics/js/canvas_funcs.mjs
+++ 02_Canvas_Basics/js/canvas_funcs.mjs
@@ -1,6 +1,17 @@
 export function initCanvas(id) {
+    // HTML-Element canvas;
     const canvas = document.getElementById(id);
+
+    // Context
     const context = canvas.getContext('2d');
     return { canvas, context };
 }
 
+export function line(ctx, x1, y1, x2, y2, strokeStyle = "#fff", lineWidth = 1) {
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
--- 02_Canvas_Basics/main_canvas.mjs
+++ 02_Canvas_Basics/main_canvas.mjs
@@ -1,6 +1,7 @@
 import * as lib from "./js/canvas_funcs.mjs";
 
 window.onload = () => {
+    // Aufruf: initialisier Canvas, id des HTM: "canvas"
     const { canvas, context } = lib.initCanvas("canvas");
 
     function resizeCanvas() {
@@ -10,11 +11,19 @@ window.onload = () => {
     resizeCanvas();
     window.addEventListener('resize', resizeCanvas);
 
-
+    let x = 0, y = 0, sx = 3, sy = 3;
+    const q = 20;
     function draw() {
         context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = "#ff5a9e";
+        context.fillRect(x - 5, y - 5, q * 2, q * 2);
+
         context.fillStyle = "#005a9e";
-        context.fillRect(100, 100, 20, 20);
+        x += sx; if (x + q > canvas.width || x < 0) sx *= -1;
+        y += sy; if (y + q > canvas.height || y < 0) sy *= -1;
+        context.fillRect(x, y, q, q + 10);
+
+        lib.line(context, canvas.width / 2, canvas.height / 2, x, y, "red", 1);
 
         window.requestAnimationFrame(draw);
     }
